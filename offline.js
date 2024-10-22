454552be93fdf779e5eb0ad1d318feb255f0528e
franko <facostabricen90@gmail.com>
2024-10-21 20:30:24 -0500

    first commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1729559909,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/cuerponivel1.png",
+		"images/paredesnivel1.png",
+		"images/hero-sheet0.png",
+		"images/cure-sheet0.png",
+		"images/enemy1-sheet0.png",
+		"images/enemy2-sheet0.png",
+		"images/enemy3-sheet0.png",
+		"images/minienemy1-sheet0.png",
+		"images/minienemy1-sheet1.png",
+		"images/minienemy2-sheet0.png",
+		"images/minienemy3-sheet0.png",
+		"images/toxic-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
